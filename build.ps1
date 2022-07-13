33bc3e85709d38de18fb2961bf8cd7ef5e3f0ae5
Austin <hireaustinhall@gmail.com>
2022-07-13 08:34:59 -0400

    Update build script to use 7zip, needed to pass firefox linter

--- build.ps1
+++ build.ps1
@@ -4,9 +4,11 @@ if (Test-Path $ZipPath) {
   Remove-Item $ZipPath
 }
 
-$compress = @{
-  Path             = "./icons", "entry.html", "main.js", "manifest.json", "popup.js", "styles.css"
-  CompressionLevel = "Fastest"
-  DestinationPath  = $ZipPath
+$7ZipPath = "C:/Program Files/7-Zip/7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+  throw "7 zip file '$7zipPath' not found"
 }
-Compress-Archive @compress
+
+Set-Alias 7zip $7zipPath
+7zip a -tzip $ZipPath "./icons", "entry.html", "main.js", "manifest.json", "popup.js", "styles.css" 
